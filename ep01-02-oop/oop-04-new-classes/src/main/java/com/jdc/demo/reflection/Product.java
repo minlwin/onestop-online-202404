b29c725d96d9f26e3bedb22dcec887280b90dfe3
Min Lwin <lwin.zawmin@gmail.com>
2024-06-17 20:13:26 +0630

    Update for Using Reflection

---
+++ ep01-02-oop/oop-04-new-classes/src/main/java/com/jdc/demo/reflection/Product.java
@@ -0,0 +1,11 @@
+package com.jdc.demo.reflection;
+
+import lombok.Data;
+
+@Data
+public class Product {
+
+	private int id;
+	private String name;
+	private int price;
+}
---
+++ ep01-02-oop/oop-04-new-classes/src/main/java/com/jdc/demo/reflection/UsingReflectionApi.java
@@ -0,0 +1,52 @@
+package com.jdc.demo.reflection;
+
+import java.lang.reflect.InvocationTargetException;
+
+public class UsingReflectionApi {
+
+	public Product getProduct()  {
+		try {
+			return Product.class.getDeclaredConstructor().newInstance();
+		} catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException
+				| NoSuchMethodException | SecurityException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public void showMethods(Product product) {
+		
+		var methods = product.getClass().getMethods();
+		
+		for(var method : methods) {
+			System.out.println(method.getName());
+		}
+	}
+	
+	public void showVariables(Product product) {
+		
+		var fields = product.getClass().getDeclaredFields();
+		
+		for(var field : fields) {
+			System.out.println(field.getName());
+		}
+	}
+	
+	public void setValue(Product product, int id, String name, int price) {
+		
+		var fields = product.getClass().getDeclaredFields();
+		
+		for(var field : fields) {
+			
+			try {
+				field.setAccessible(true);
+				switch(field.getName()) {
+				case "id" -> field.set(product, id);
+				case "name" -> field.set(product, name);
+				case "price" -> field.set(product, price);
+				}
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
+}
