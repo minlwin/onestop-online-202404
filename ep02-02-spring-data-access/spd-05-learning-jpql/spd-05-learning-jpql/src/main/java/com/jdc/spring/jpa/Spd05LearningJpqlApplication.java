89f1d84a0a8f06e4415df8df652b96e2804ac43e
Min Lwin <lwin.zawmin@gmail.com>
2024-11-01 21:00:03 +0630

    Share Project

---
+++ ep02-02-spring-data-access/spd-05-learning-jpql/spd-05-learning-jpql/src/main/java/com/jdc/spring/jpa/Spd05LearningJpqlApplication.java
@@ -0,0 +1,13 @@
+package com.jdc.spring.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spd05LearningJpqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spd05LearningJpqlApplication.class, args);
+	}
+
+}
---
+++ ep02-02-spring-data-access/spd-05-learning-jpql/spd-05-learning-jpql/src/test/java/com/jdc/spring/jpa/Spd05LearningJpqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdc.spring.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spd05LearningJpqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
