05247ae0e558a8543af030e3543bb125700431e0
Min Lwin <lwin.zawmin@gmail.com>
2025-06-02 21:40:21 +0630

    Update Preserve and Reset State

--- ep03-02-react/rew-05-state-management/src/pages/structure/flat-structure-state.tsx
+++ ep03-02-react/rew-05-state-management/src/pages/structure/flat-structure-state.tsx
@@ -5,6 +5,7 @@ export default function FlatStructure() {
 
     const [flatPlan, setFlatPlan] = useState<Record<number, FlatPlan>>(flatTravelPlan)
     const basePlan = flatPlan[0]
+    const [selectedPlan, setSelectedPlan] = useState<number>(1)
 
     const handleOnVisit = (placeId:number, parentId:number) => {
         const parent = flatPlan[parentId]
@@ -18,22 +19,32 @@ export default function FlatStructure() {
             <h3>Flat State</h3>
 
             <section className="accordion">
-                {basePlan.childIds.map(childId => <PlaceComponent key={childId} planId={childId} parentId={0} planRecord={flatPlan} onVisit={handleOnVisit} />)}
+                {basePlan.childIds.map(childId => <PlaceComponent 
+                    key={childId} 
+                    planId={childId} 
+                    parentId={0} 
+                    planRecord={flatPlan} 
+                    selectedId={selectedPlan}
+                    onVisit={handleOnVisit} 
+                    onClick={setSelectedPlan} />)}
             </section>
         </>
     )
 }
 
-function PlaceComponent({planId, parentId, planRecord, onVisit} 
+function PlaceComponent({planId, parentId, planRecord, selectedId, onVisit, onClick} 
     : {
         planId : number, 
         parentId : number,
         planRecord : Record<number, FlatPlan>,
-        onVisit: (placeId:number, parentId:number) => void
+        selectedId: number,
+        onVisit: (placeId:number, parentId:number) => void,
+        onClick: (placeId:number) => void,
     }) {
     
     const plan = planRecord[planId]
-
+    const show = (planId === selectedId || hasChildForShow(planRecord, planId, selectedId))
+ 
     if(plan.childIds.length == 0) {
         return (
             <div className="card mb-2">
@@ -49,15 +60,43 @@ function PlaceComponent({planId, parentId, planRecord, onVisit}
     
     return (
         <div className="accordion-item">
-            <h5 className="accordion-header">
-                <button className={`accordion-button ${plan.id == 1 ? '' : 'collapsed'}`} data-bs-toggle="collapse" data-bs-target={`#collapse-${planId}`}>{plan.title}</button>
+            <h5 className="accordion-header" onClick={() => onClick(planId)}>
+                <button className={`accordion-button ${show ? '' : 'collapsed'}`} data-bs-toggle="collapse" data-bs-target={`#collapse-${planId}`}>{plan.title}</button>
             </h5>
 
-            <div id={`collapse-${plan.id}`} className={`accordion-collapse collapse ${planId == 1 ? 'show' : ''}`}>
+            <div id={`collapse-${plan.id}`} className={`accordion-collapse collapse ${show ? 'show' : ''}`}>
                 <div className="accordion-body">
-                    {plan.childIds.map(childId => <PlaceComponent key={childId} planId={childId} parentId={planId} planRecord={planRecord} onVisit={onVisit} />)}
+                    {plan.childIds.map(childId => <PlaceComponent 
+                        key={childId} 
+                        planId={childId} 
+                        parentId={planId} 
+                        planRecord={planRecord} 
+                        selectedId={selectedId}
+                        onVisit={onVisit}
+                        onClick={onClick} />)}
                 </div>
             </div>
         </div>
     )
+}
+
+function hasChildForShow(planRecord : Record<number, FlatPlan>, planId:number, selectedId:number) {
+    
+    const plan = planRecord[planId]
+
+    if(plan.childIds.length > 0) {
+        for(const childId of plan.childIds) {
+            if(childId === selectedId) {
+                return true
+            }
+
+            const result = hasChildForShow(planRecord, childId, selectedId)
+
+            if(result) {
+                return true
+            }
+        }
+    }
+    
+    return false
 }
