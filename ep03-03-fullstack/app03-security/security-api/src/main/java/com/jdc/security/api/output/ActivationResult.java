8cf6022e0a638da679da11d4f72fff36c946ffd5
minlwin <lwin.zawmin@gmail.com>
2026-03-12 23:32:28 +0630

    Update Activation Endpoints

---
+++ ep03-03-fullstack/app03-security/security-api/src/main/java/com/jdc/security/api/output/ActivationResult.java
@@ -0,0 +1,6 @@
+package com.jdc.security.api.output;
+
+public record ActivationResult(
+		String message) {
+
+}
--- ep03-03-fullstack/app03-security/security-api/src/main/java/com/jdc/security/model/service/AccountService.java
+++ ep03-03-fullstack/app03-security/security-api/src/main/java/com/jdc/security/model/service/AccountService.java
@@ -1,5 +1,6 @@
 package com.jdc.security.model.service;
 
+import java.time.LocalDateTime;
 import java.util.concurrent.ThreadLocalRandom;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,12 +8,15 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.jdc.security.api.input.ActivationForm;
 import com.jdc.security.api.input.SignUpForm;
+import com.jdc.security.api.output.ActivationResult;
 import com.jdc.security.api.output.SignUpResult;
 import com.jdc.security.model.repo.AccountRepo;
 import com.jdc.security.utils.exception.AppBusinessException;
 
 @Service
+@Transactional
 public class AccountService {
 	
 	@Autowired
@@ -22,7 +26,6 @@ public class AccountService {
 	@Autowired
 	private AccountComfirmationService comfirmationService;
 
-	@Transactional
 	public SignUpResult signUp(SignUpForm form) {
 		
 		if(accountRepo.findOneByEmail(form.email()).isPresent()) {
@@ -43,9 +46,29 @@ public class AccountService {
 		return new SignUpResult(entity.getId(), "Please check your email.");
 	}
 
+	public ActivationResult activate(ActivationForm form) {
+		
+		var account = accountRepo.findById(form.userId())
+				.orElseThrow(() -> new AppBusinessException("Invalid Account ID."));
+		
+		if(null != account.getActivatedAt()) {
+			throw new AppBusinessException("Account had been activated..");
+		}
+		
+		if(!passwordEncoder.matches(form.otpCode(), account.getPassword())) {
+			throw new AppBusinessException("Invalid OTP Code.");
+		}
+		
+		account.setPassword(passwordEncoder.encode(form.password()));
+		account.setActivatedAt(LocalDateTime.now());
+		
+		return new ActivationResult("Your account has been activated.");
+	}
+
 	private String generateOtp() {
 		var code = ThreadLocalRandom.current().nextInt(999999);
 		return "%06d".formatted(code);
 	}
+
 	
 }
