22ed20081d599127ed31a53d93ab3a19f96f48b8
minlwin <lwin.zawmin@gmail.com>
2026-03-19 11:26:50 +0630

    Update Activation

--- ep03-03-fullstack/app03-security/security-api/src/main/java/com/jdc/security/api/ActivationApi.java
+++ ep03-03-fullstack/app03-security/security-api/src/main/java/com/jdc/security/api/ActivationApi.java
@@ -1,25 +1,5 @@
 package com.jdc.security.api;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.jdc.security.api.input.ActivationForm;
-import com.jdc.security.api.output.ActivationResult;
-import com.jdc.security.model.service.AccountService;
-
-@RestController
-@RequestMapping("auth/activate")
 public class ActivationApi {
-	
-	@Autowired
-	private AccountService service;
 
-	@PostMapping
-	ActivationResult activate(@Validated @RequestBody ActivationForm form) {
-		return service.activate(form);
-	}
 }
